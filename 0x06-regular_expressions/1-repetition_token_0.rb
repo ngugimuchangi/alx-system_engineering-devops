1a4a65ce59e31fd70cba2e2705328a766d57e718
ngugimuchangi <tech.curious14@gmail.com>
2022-08-23 10:13:40 +0000

    task 3: repetition token #2

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# Regular expression to match 'School'
+# Regular expression to match a reptition of t in range 2 to 5
 
 puts ARGV[0].scan(/hbt{2,5}n$/).join
--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# Regular expression to match 'School'
+# Regular expression to match zero or one incidence of 'b'
 
 puts ARGV[0].scan(/hb?tn$/).join

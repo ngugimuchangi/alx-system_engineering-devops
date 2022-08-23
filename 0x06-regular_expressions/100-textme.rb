c9af422fe1e68994bf6df2bb5e61e514dd45dddd
ngugimuchangi <tech.curious14@gmail.com>
2022-08-23 11:50:53 +0000

    fix: work when fields are missing

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,5 @@
 # Regular expression to match a string a 10 digit phone number
 
 # puts ARGV[0].scan(/from:(.*])\b.*to:(.\d*)\b.*flags:(.*\w)\]/).join(",")
-puts ARGV[0].scan(/from:(.\w*)\].*to:(.\w*).*flags:(.*\d)\]/).join(",")
+# puts ARGV[0].scan(/from:(.\w*)\].*to:(.\w*).*flags:(.*\d)\]/).join(",")
+puts ARGV[0].scan(/from:(.*)\].*to:(.*)\].*flags:(.*)\].*\[msg/).join(",")

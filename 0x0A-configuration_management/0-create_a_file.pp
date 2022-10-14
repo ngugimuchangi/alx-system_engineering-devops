e064764d9da5daf9d8be1aa3612a3cda9f87da7e
ngugimuchangi <tech.curious14@gmail.com>
2022-10-14 13:05:04 +0000

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file in /tmp
 
-file {'school':
-path    => '/tmp/school',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'wwd-data',
-content => 'I love Puppet'
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'wwd-data',
+  content => 'I love Puppet'
 }

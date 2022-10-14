834576f7e6548d5a2fb795fc186e0c50717afcbf
ngugimuchangi <tech.curious14@gmail.com>
2022-10-14 11:49:29 +0000

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
-# Script to create a file in /temp directory
+# Create a file in /tmp
 
 file {'school':
-ensure  => 'present',
 path    => '/tmp/school',
 mode    => '0744',
 owner   => 'www-data',

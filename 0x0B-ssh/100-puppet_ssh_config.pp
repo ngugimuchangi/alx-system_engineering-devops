6c37ed6c293cd35b864ab3b867cb91b6f9a124de
ngugimuchangi <tech.curious14@gmail.com>
2022-10-17 02:41:03 +0000

    rectified file name

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Manifest to change ssh login configuration
 
 exec { 'ssh_login':
-  command => 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+  command => 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
   path    => '/usr/bin/'
 }

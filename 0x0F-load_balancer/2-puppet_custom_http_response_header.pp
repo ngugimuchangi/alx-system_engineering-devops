13246d78b8093bdf9edca2d71175461c08933a55
ngugimuchangi <tech.curious14@gmail.com>
2022-10-25 04:13:35 +0000

    search and replace :search:

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -16,7 +16,7 @@ exec { 'start':
 }
 
 exec { 'config':
-  command  => 'sudo sed -i "11i\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf',
+  command  => 'sudo sed -i s@"http {"@"http {\n\tadd_header X-Served-By \$HOSTNAME;"@g /etc/nginx/nginx.conf',
   provider => 'shell',
 }
 

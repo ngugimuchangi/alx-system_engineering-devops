a6d5dd3030aeae21f0b1bb40ee2e7b7ce47e140e
ngugimuchangi <tech.curious14@gmail.com>
2022-10-25 07:23:05 +0000

    puppet :tada:

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,21 @@
 # Puppet manifest to install nginx and add custom header
 
 exec { 'update':
-  command  => 'sudo apt -y update',
-  provider => 'shell'
+  command  => 'sudo apt -y update && sudo apt -y upgrade',
+  provider => 'shell',
+  before   => Exec['nginx'],
 }
 
 exec { 'nginx':
-  command  => 'sudo apt-get -y install nginx',
-  provider => 'shell'
+  command  => 'sudo apt -y install nginx',
+  provider => 'shell',
+  before   => Exec['config'],
 }
 
 exec { 'config':
   command  => 'sudo sed -i s@"http {"@"http {\n\tadd_header X-Served-By \$HOSTNAME;"@g /etc/nginx/nginx.conf',
   provider => 'shell',
-  notify   => Exec['restart']
+  before   => Exec['restart'],
 }
 
 exec { 'restart':

bc69ed8f24aff80c7f91b588de241cb3dfa03dcf
ngugimuchangi <tech.curious14@gmail.com>
2022-12-02 08:57:14 +0000

    minor function edit and README edit

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -11,40 +11,37 @@ def count_words(subreddit, word_list, key_words={}, count={}, after=None):
     """
     if not count:
         key_words = {word.lower(): 0 for word in word_list}
-        word_list_cpy = str(word_list).lower().strip("[]")
-        word_list_cpy = word_list_cpy.replace(',', '')
-        word_list_cpy = word_list_cpy.replace("'", '').split()
-        count = {word.lower(): word_list_cpy.count(word.lower())
+        word_list = [word.lower() for word in word_list]
+        count = {word.lower(): word_list.count(word.lower())
                  for word in word_list}
 
-    if subreddit and type(subreddit) is str and word_list:
-        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-        params = {'after': after, 'limit': 100}
-        headers = {'user-agent': 'my-app/0.0.1'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    params = {'after': after, 'limit': 100}
+    headers = {'user-agent': 'my-app/0.0.1'}
 
-        req = get(url, params=params, headers=headers, allow_redirects=False)
-        #  get data if request was successful
-        if req.status_code == 200:
-            data = req.json().get('data')
-            after = data.get('after')
-            posts = data.get('children')
+    req = get(url, params=params, headers=headers, allow_redirects=False)
+    #  get data if request was successful
+    if req.status_code == 200:
+        data = req.json().get('data')
+        after = data.get('after')
+        posts = data.get('children')
 
-            #  get count of key words in each title
-            for post in posts:
-                title = post.get('data').get('title').lower()
-                words = title.split()
-                for word in key_words.keys():
-                    key_words[word] += words.count(word)
+        #  get count of key words in each title
+        for post in posts:
+            title = post.get('data').get('title').lower()
+            words = title.split()
+            for word in key_words.keys():
+                key_words[word] += words.count(word)
 
-            #  call recursive function if there's more data
-            if after:
-                return count_words(subreddit, word_list,
-                                   key_words, count, after)
-            else:
-                for key in key_words.keys():
-                    key_words[key] *= count[key]
-                key_words = sorted(key_words.items(),
-                                   key=lambda item: (-item[1], item[0]))
-                for item in key_words:
-                    if item[1] > 0:
-                        print("{}: {}".format(item[0], item[1]))
+        #  call recursive function if there's more data
+        if after:
+            return count_words(subreddit, word_list,
+                               key_words, count, after)
+        else:
+            for key in key_words.keys():
+                key_words[key] *= count[key]
+            key_words = sorted(key_words.items(),
+                               key=lambda item: (-item[1], item[0]))
+            for item in key_words:
+                if item[1] > 0:
+                    print("{}: {}".format(item[0], item[1]))
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -1,5 +1,15 @@
 # 0x16. API advanced
 
 ## About
+More API practice
+  * Working with Reddit API
 
 ## Tasks
+0. Function that queries the [Reddit API](https://www.reddit.com/dev/api/) and returns the number of subscribers for a given subreddit
+    * [0-subs.py](0-subs.py)
+1. Function that queries [Reddit API](https://www.reddit.com/dev/api/) and prints the title of the first 10 hot post listed for a given subreddit
+    * [1-top_ten.py](1-top_ten.py)
+2. Recursive function that queris [Reddit API](https://www.reddit.com/dev/api/) and returns a list containing the titles of all hot articles for a given subreddit.
+    * [2-recurse.py](2-recurse.py)
+3. Recursive function that queries [Reddit API](https://www.reddit.com/dev/api/), parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces.
+    * [100-count.py](100-count.py)

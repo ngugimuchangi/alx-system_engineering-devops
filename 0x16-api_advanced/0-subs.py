2b74dc5ff422dcd32e1fba158a9e26945b0d32d4
ngugimuchangi <tech.curious14@gmail.com>
2022-12-02 05:14:28 +0000

    fixed return value for invalid subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,11 @@ from requests import get
 def number_of_subscribers(subreddit):
     """ function to get subscriber count"""
     if subreddit and type(subreddit) is str:
+        subscribers = 0
         url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
         headers = {'user-agent': 'my-app/0.0.1'}
         req = get(url, headers=headers)
         if req.status_code == 200:
             data = req.json()
             subscribers = data.get('data', {}).get('subscribers', 0)
-            return subscribers
+        return subscribers

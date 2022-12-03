dbab526041123faf93f382f71719a58b176f3890
ngugimuchangi <tech.curious14@gmail.com>
2022-12-03 10:44:35 +0000

    fixed resource type name

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Manifest to fix apache configuration
-file { 'fix config file':
+exec { 'fix config file':
   command =>  "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => '/bin'
 }

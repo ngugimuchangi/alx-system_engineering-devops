1d4e0de4801631aeb032224be4efd972da9648d2
ngugimuchangi <tech.curious14@gmail.com>
2022-12-03 10:43:29 +0000

    puppet-lint fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Manifest to fix apache configuration
 file { 'fix config file':
-  command  =>  "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path => '/bin'
+  command =>  "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin'
 }

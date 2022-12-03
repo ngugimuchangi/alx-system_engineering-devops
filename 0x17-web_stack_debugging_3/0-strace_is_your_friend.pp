1c115ae1d9254a090dc7df5a5cead158adfc22b1
ngugimuchangi <tech.curious14@gmail.com>
2022-12-03 10:41:09 +0000

    switched to file configuration fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Manifest to fix apache configuration
-file { 'index.html':
-  ensure =>  present,
-  path   => '/var/www/html/index.html'
+file { 'fix config file':
+  command  =>  "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  provider => 'shell'
 }

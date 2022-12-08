2c7071ca8a06fc5052a42ccc78248da41e512d1d
ngugimuchangi <tech.curious14@gmail.com>
2022-12-08 10:30:16 +0000

    increased file limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Manifest to fix Nginx open file limit configuration
 exec { 'nginx fix':
-  command => 'sed -i s/15/500/g /etc/default/nginx',
+  command => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx",
   path    =>  '/bin'
 }
 

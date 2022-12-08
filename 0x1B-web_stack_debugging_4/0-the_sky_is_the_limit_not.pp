661cdc03ab27bf768407184723758e76f449d3f3
ngugimuchangi <tech.curious14@gmail.com>
2022-12-08 10:35:06 +0000

    check if config file exist before running manifest

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,6 @@
 # Manifest to fix Nginx open file limit configuration
 exec { 'nginx fix':
-  command => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx",
-  path    =>  '/bin'
-}
-
-exec { 'nginx restart':
-  command  => 'service nginx restart',
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx;  service nginx restart",
   provider => 'shell'
 }
